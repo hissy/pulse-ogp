2176c39061dae39bf16bcd46816c79ab10909c99
Takuro Hishikawa <hishikawa@concrete5.co.jp>
2016-10-17 22:29:27 +0900

    Initial Commit

---
+++ inc/plugins/ogp.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hishikawa
+ * Date: 2016/10/17
+ * Time: 21:43
+ */
+class Ogp
+{
+    protected static $tags = array();
+
+    public static function add($property, $content)
+    {
+        static::$tags[$property] = $content;
+    }
+
+    public static function render()
+    {
+        global $path, $page, $page_title, $page_desc, $url_title, $blog_prefix;
+        foreach (static::$tags as $property => $content) {
+            if ($property == 'image') {
+                $content = static::getBaseURL() . $path . '/'. $content;
+            }
+            static::print_tag($property, $content);
+        }
+
+        if (!static::exists('url')) {
+            if ($url_title && $blog_prefix) {
+                $url = static::getBaseURL() . $path . '/' . $blog_prefix . '-' . strtolower($url_title);
+            } else {
+                $url = static::getBaseURL() . $path . '/' . $page;
+            }
+            static::print_tag('url', $url);
+        }
+
+        if (!static::exists('type')) {
+            static::print_tag('type', 'article');
+        }
+
+        if (!static::exists('title')) {
+            static::print_tag('title', $page_title);
+        }
+
+        if (!static::exists('description')) {
+            static::print_tag('description', $page_desc);
+        }
+    }
+
+    private static function print_tag($property, $content)
+    {
+        $property = htmlspecialchars($property, ENT_QUOTES, 'UTF-8');
+        $content = htmlspecialchars($content, ENT_QUOTES, 'UTF-8');
+        echo sprintf("<meta property=\"og:%s\" content=\"%s\" />\n", $property, $content);
+    }
+
+    private static function exists($property)
+    {
+        return array_key_exists($property, static::$tags);
+    }
+
+    private static function isSecure()
+    {
+        $isSecure = false;
+        if ($_SERVER['HTTPS'] && strtolower($_SERVER['HTTPS']) == 'on') {
+            $isSecure = true;
+        }
+        return $isSecure;
+    }
+
+    private static function getScheme()
+    {
+        return (static::isSecure()) ? 'https' : 'http';
+    }
+
+    private static function getHost()
+    {
+        return $_SERVER['HTTP_HOST'];
+    }
+
+    private static function getBaseURL()
+    {
+        global $ogp_base_url;
+        return (isset($ogp_base_url)) ? $ogp_base_url : static::getScheme() . '://' . static::getHost();
+    }
+
+}
+
+function ogp_render() {
+    Ogp::render();
+}
---
+++ inc/tags/ogp.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: hishikawa
+ * Date: 2016/10/17
+ * Time: 21:40
+ */
+include_once('inc/plugins/ogp.php');
+
+Ogp::add($tag_var1, $tag_var2);
